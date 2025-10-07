14de18432dcf8d59e63417bb19783d5a641c60b7
Enrique Alvarado <alvaradoenrique257@gmail.com>
2025-10-06 20:48:54 -0600

    Creacion y estilos de componente Call To Action Button 🆕🔷

---
+++ src/components/CTAButton.js
@@ -0,0 +1,9 @@
+
+
+export const CTAButton = ({ children }) => {
+  return (
+    <button className="bg-gradient-to-r from-[#4551d0] to-[#3c9fd3] text-white text-sm font-medium px-5 py-3 rounded-full shadow-lg hover:opacity-90 transition">
+      {children}
+    </button>
+  )
+}
