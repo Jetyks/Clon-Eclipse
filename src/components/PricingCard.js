f1463b99c2f3f53565b7e3039c3947d6efcfc53e
Enrique Alvarado <alvaradoenrique257@gmail.com>
2025-10-20 20:26:51 -0600

    Clase condicional para borde en PricingCard segun isFeatured; false or true 🔷🔥

--- src/components/PricingCard.js
+++ src/components/PricingCard.js
@@ -1,7 +1,12 @@
 
-export const PricingCard = ({name, price, tagline, features}) => {
+export const PricingCard = ({name, price, tagline, features, isFeatured}) => {
+    const featuredBorderClass = "border-[#3c9fd3]";
+    const defaultBorderClass = "border-semitransparent"
+    
+    const cardBorder = isFeatured ? featuredBorderClass : defaultBorderClass;
+
   return (
-    <div className="h-full border rounded-xl border-[#3c9fd3] default-white">
+    <div className={`h-full border rounded-xl ${cardBorder} default-white`}>
         <div className='border-b-semitransparent py-9 space-y-3'>
             <h4 className="text-[#3c9fd3] text-lg font-medium">{name}</h4>
             <p className="text-3xl font-medium">{price} <span className="opacity-65 text-xl">/mo</span></p>
