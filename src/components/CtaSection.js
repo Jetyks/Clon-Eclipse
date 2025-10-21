bccd33da75e7e87f44bcac4d5f2141692f444994
Enrique Alvarado <alvaradoenrique257@gmail.com>
2025-10-20 20:53:59 -0600

    Correcion para color azul en botones CTA con props 🔷🔨🔥

--- src/components/CtaSection.js
+++ src/components/CtaSection.js
@@ -15,7 +15,7 @@ export const CtaSection = () => {
         <div className="z-20">
             <h3 className="title-2 mb-2">Build a head start to your finances</h3>
             <p className="opacity-80">Eclipse is the perfect answer! Our platform enables you to create wealth, manage <br/> your expenses, stay up to date with the latest financial news, and get ready for <br/> upcoming tax season.</p>
-            <div className="mt-5"><CTAButton>Start now</CTAButton></div>
+            <div className="mt-5"><CTAButton colorBlue = {true}>Start now</CTAButton></div>
         </div>
     </section>
   )
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ export const Hero = () => {
     <section className="text-center py-32 px-60">
         <h1 className="font-extrabold text-6xl text-transparent bg-clip-text bg-gradient-to-b from-[var(--default-white)] to-gray-500">All-in-one Finance App <span className="text-transparent bg-clip-text bg-gradient-to-b from-[var(--default-white)] to-gray-500 opacity-90">you always wanted</span></h1>
         <p className="my-5 px-28 default-white opacity-80">Gain control of your finances with our all-in-one finance app. Track your spending, find investment opportunities, and learn.</p>
-        <CTAButton>Get Started</CTAButton>
+        <CTAButton colorBlue = {true}>Get Started</CTAButton>
     </section>
   )
 }
